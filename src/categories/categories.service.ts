52832f95b7f1191947184807fb3300ebcd6308af
sabidjanovv <sabidjanovv@gmail.com>
2025-05-04 19:56:04 +0500

    categories  added

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './schemas/category.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const category = new this.categoryModel(createCategoryDto);
+    return await category.save();
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll(): Promise<Category[]> {
+    return await this.categoryModel.find().lean();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: string): Promise<Category> {
+    const category = await this.categoryModel.findById(id).lean();
+    if (!category) {
+      throw new NotFoundException('Kategoriya topilmadi');
+    }
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(
+    id: string,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    const updated = await this.categoryModel.findByIdAndUpdate(
+      id,
+      updateCategoryDto,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    if (!updated) {
+      throw new NotFoundException('Kategoriya topilmadi');
+    }
+
+    return updated;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: string): Promise<{ message: string }> {
+    const result = await this.categoryModel.findByIdAndDelete(id);
+    if (!result) {
+      throw new NotFoundException('Kategoriya topilmadi');
+    }
+    return { message: 'Kategoriya muvaffaqiyatli o‘chirildi' };
   }
 }
--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCategoryDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCategoryDto {
+  @ApiProperty({ example: 'Programming', description: 'Kategoriya nomi' })
+  @IsString()
+  @IsNotEmpty({ message: 'Kategoriya nomi bo‘sh bo‘lishi mumkin emas' })
+  name: string;
+}
